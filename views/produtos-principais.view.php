b49b891ac73e9403eada14950eee64e074b6db1b
RonaldoVMDS <ronaldovinicius.machado@hotmail.com>
2022-05-16 22:31:36 -0300

    Produtos principais aparecendo randomicamente pt 2

--- views/produtos-principais.view.php
+++ views/produtos-principais.view.php
@@ -1,25 +1,30 @@
 <?php
-    require ('conexao.php');
-    $bd = Conexao::get();
-    $query = $bd -> prepare("SELECT * FROM produto");
-    $query -> execute();
-    $produtos = $query -> fetchAll(PDO::FETCH_OBJ);
+require('conexao.php');
+$bd = Conexao::get();
+$query = $bd->prepare("SELECT * FROM produto");
+$query->execute();
+$produtos = $query->fetchAll(PDO::FETCH_OBJ);
+$produtosRandom = $produtos;
+
+shuffle($produtosRandom);
+
 ?>
 <main class="container">
     <!-- Primeira linha de conteúdos -->
     <div class="row g-3">
-    
-        <?php for ($controle = 0; $controle < 12; $controle++) : ?> <!-- Laço de repetição da estrutura dos produtos, imprime os 8 primeiros produtos -->
-            <div class="col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch"> 
+
+        <?php for ($controle = 0; $controle < 12; $controle++) : ?>
+            <!-- Laço de repetição da estrutura dos produtos, imprime os 8 primeiros produtos -->
+            <div class="col-lg-3 col-md-4 col-sm-6 d-flex align-items-stretch">
                 <!-- A estrutura dos produtos do array é: [produto['imagem', (float)preco, 'nome', 'descrição','qtde em estoque']] -->
                 <div class="card text-center bg-light">
                     <a href="produto.php">
-                    <img src="<?= $produtos[$controle]->prod_imagem ?>" class="card-img" style="cursor: pointer;" alt="">
+                        <img src="<?= $produtosRandom[$controle]->prod_imagem ?>" class="card-img" style="cursor: pointer;" alt="">
                     </a>
-                    <div class="card-header">R$ <?= $produtos[$controle]->prod_preco ?></div>
+                    <div class="card-header">R$ <?= $produtosRandom[$controle]->prod_preco ?></div>
                     <div class="card-body">
-                        <h4 class="card-title text-success description-size-limiter-1-line"><?= $produtos[$controle]->prod_nome ?></h4>
-                        <p class="card-text description-size-limiter-2-lines"><?= $produtos[$controle]->prod_descricao ?></p>
+                        <h4 class="card-title text-success description-size-limiter-1-line"><?= $produtosRandom[$controle]->prod_nome ?></h4>
+                        <p class="card-text description-size-limiter-2-lines"><?= $produtosRandom[$controle]->prod_descricao ?></p>
                     </div>
                 </div>
             </div>
